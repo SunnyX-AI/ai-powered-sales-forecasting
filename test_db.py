3f925d78cd3fa588d7332b4af37d988402057d7d
Bonaventure Osuide <bonaventureosuide@gmail.com>
2026-04-07 16:00:18 +0100

    Added code to always connect to Supabase

--- test_db.py
+++ test_db.py
@@ -1,21 +1,37 @@
+# # from src.data.db_connection import run_query
+
+# # query = """
+# # SELECT *
+# # FROM core.fact_sales
+# # LIMIT 5;
+# # """
+
+# # df = run_query(query)
+# # print(df)
+
 # from src.data.db_connection import run_query
 
 # query = """
 # SELECT *
-# FROM core.fact_sales
+# FROM analytics.vw_daily_store_sales
 # LIMIT 5;
 # """
 
 # df = run_query(query)
 # print(df)
+# import pandas as pd
+# from sqlalchemy import create_engine
+ 
+# engine = create_engine("postgresql://sfs_reader:Chinenyennabude123@db.ogkdfmkybqtrsglcizzt.supabase.co:5432/postgres")
+ 
+# df = pd.read_sql("SELECT * FROM core.fact_sales LIMIT 200", engine)
 
-from src.data.db_connection import run_query
+from sqlalchemy import create_engine
+import pandas as pd
 
-query = """
-SELECT *
-FROM analytics.vw_daily_store_sales
-LIMIT 5;
-"""
+engine = create_engine(
+    "postgresql://sfs_user:Chinenyennabude123@aws-0-eu-west-2.pooler.supabase.com:6543/postgres"
+)
 
-df = run_query(query)
-print(df)
+df = pd.read_sql("SELECT * FROM core.fact_sales LIMIT 5", engine)
+print(df.head())
