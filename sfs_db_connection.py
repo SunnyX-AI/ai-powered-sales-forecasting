609c34f5be28a89e8d513d72b617bc9dcff2b047
Bonaventure Osuide <bonaventureosuide@gmail.com>
2026-04-07 19:52:35 +0100

    Edited

---
+++ sfs_db_connection.py
@@ -0,0 +1,39 @@
+import pandas as pd
+from sqlalchemy import create_engine
+from urllib.parse import quote_plus
+
+host = "YOUR_HOST" # add host
+port = 5432
+database = "postgres" 
+user = "sfs_user"   # add user
+password = quote_plus("YOUR_PASSWORD")  # add password
+
+engine = create_engine(
+    f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}",
+    pool_pre_ping=True
+)
+
+df = pd.read_sql("SELECT * FROM core.fact_customer_activity LIMIT 5", engine)
+print(df.head())
+
+
+# host = "aws-1-eu-central-1.pooler.supabase.com"
+# user = "sfs_user.ogkdfmkybqtrsglcizzt"
+# password = "Chinenyennabude123"
+
+# Table names "core"
+
+# e.g core.dim_stores
+
+
+# dim_calendar
+# dim_policies
+# dim_products
+# dim_stores
+# fact_customer_activity
+# fact_inventory
+# fact_promotions
+# fact_restrictions_events
+# fact_sales
+# fact_store_operations
+# fact_weather
